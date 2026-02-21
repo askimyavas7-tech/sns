7a6a0a45e83a2728a9ff6319e4e21110e5e14c8c
askimyavas7-tech <askimyavas7@gmail.com>
2026-02-22 00:23:13 +0300

    Update database.py

--- maythusharmusic/utils/database/database.py
+++ maythusharmusic/utils/database/database.py
@@ -417,8 +417,8 @@ async def get_lang(chat_id: int) -> str:
     if not mode:
         lang = await langdb.find_one({"chat_id": chat_id})
         if not lang:
-            langm[chat_id] = "my"
-            return "my"
+            langm[chat_id] = "tr"
+            return "tr"
         langm[chat_id] = lang["lang"]
         return lang["lang"]
     return mode
